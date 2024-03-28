01185535b4af98edbce74cdefe15f9a459bb29ff
linelie00 <dingpawn@gmail.com>
2024-03-28 14:04:59 +0900

    초기 설정 및 페이지 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
+import SessionRoomPage from './SessionRoomPage';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/" element={<SessionRoomPage />} />
+              {/* 다른 라우트들 추가 */}
+            </Routes>
+          </Router>
+        </div>
+    );
 }
 
 export default App;
