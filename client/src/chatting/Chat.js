92f75d8b81a649ba2f0017b0825747809fb02c33
linelie00 <dingpawn@gmail.com>
2024-04-15 00:54:26 +0900

    [Feat]: 채팅 기능 추가

--- client/src/chatting/Chat.js
+++ client/src/chatting/Chat.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import './chat.css';
 
-const Chat = () => {
+const socket = io();
+
+function ChatApp() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim() !== '') {
+      socket.emit('chat message', input);
+      setInput('');
+    }
+  };
+
+  useEffect(() => {
+    socket.on('chat message', (msg) => {
+      setMessages([...messages, msg]);
+    });
+
+    return () => {
+      socket.off('chat message');
+    };
+  }, [messages]);
+
   return (
     <div>
-        <h1>chat</h1>
+      <ul id="messages">
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+      <form id="form" onSubmit={handleMessageSubmit}>
+        <input id="input" autoComplete="off" type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
-};
+}
 
-export default Chat;
+export default ChatApp;
