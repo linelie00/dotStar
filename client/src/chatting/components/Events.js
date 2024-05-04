1caa13eef039ab9d0fbf510a75b0f4bd2e8b9b5d
linelie00 <dingpawn@gmail.com>
2024-05-04 22:59:39 +0900

    [Feat]:줄바꿈 처리

--- client/src/chatting/components/Events.js
+++ client/src/chatting/components/Events.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown from 'react-markdown';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { socket } from '../socket';
 
 export function Events() {
@@ -9,41 +9,50 @@ export function Events() {
 
   useEffect(() => {
     socket.on('receive message', (data) => {
-      // 서버로부터 데이터를 받아와서 events 상태에 추가
-      setEvents(prevEvents => [...prevEvents, data]);
+      setEvents((prevEvents) => [...prevEvents, data]);
     });
 
-    // 컴포넌트가 언마운트될 때 소켓 이벤트 리스너 제거
     return () => {
       socket.off('receive message');
     };
   }, []);
 
+  const components = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+        <SyntaxHighlighter
+          {...props}
+          children={String(children).replace(/\n$/, '')}
+          style={dark}
+          language={match[1]}
+          PreTag="div"
+        />
+      ) : (
+        <code {...props} className={className}>
+          {children}
+        </code>
+      );
+    },
+    p: ({ node, children }) => parseNewLines(children),
+  };
+
+  // 줄바꿈을 처리하는 함수
+  function parseNewLines(text) {
+    return text.split('\n').map((line, index) => (
+      <React.Fragment key={index}>
+        {line}
+        <br />
+      </React.Fragment>
+    ));
+  }
+
   return (
     <ul>
       {events.map((event, index) => (
-        <ReactMarkdown
-        components={{
-          // 코드 블록을 하이라이트하는 컴포넌트
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighlighter
-                {...props}
-                children={String(children).replace(/\n$/, '')}
-                style={dark}
-                language={match[1]}
-                PreTag="div"
-              />
-            ) : (
-              <code {...props} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}>
-        {event}
-      </ReactMarkdown>
+        <ReactMarkdown key={index} components={components}>
+          {event}
+        </ReactMarkdown>
       ))}
     </ul>
   );
