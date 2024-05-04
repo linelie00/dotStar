f24286bb6c5a9933792145f116eb1ce8f9ca2a6c
linelie00 <dingpawn@gmail.com>
2024-05-04 22:37:25 +0900

    [Feat]:마크다운 문법 추가

--- client/src/chatting/components/Events.js
+++ client/src/chatting/components/Events.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import ReactMarkdown from "react-markdown";
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import { socket } from '../socket';
 
 export function Events() {
@@ -19,7 +22,28 @@ export function Events() {
   return (
     <ul>
       {events.map((event, index) => (
-        <li key={index}>{event}</li>
+        <ReactMarkdown
+        components={{
+          // 코드 블록을 하이라이트하는 컴포넌트
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                children={String(children).replace(/\n$/, '')}
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}>
+        {event}
+      </ReactMarkdown>
       ))}
     </ul>
   );
