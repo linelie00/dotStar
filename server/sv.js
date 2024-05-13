b96c149082718aeaa2ca452c2d60a7be150a9af3
linelie00 <dingpawn@gmail.com>
2024-05-13 13:11:14 +0900

    [Feat]:캐릭터 id 추가

--- server/sv.js
+++ server/sv.js
@@ -1,4 +1,3 @@
-// back/app.js
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
@@ -17,17 +16,11 @@ io.on("connection", (socket) => {
     console.log('disconnected');
   });
 
-  socket.on('message', (data, callback) => {
-    console.log('Received data from client:', data);
-
-    // 여기에 클라이언트로부터 받은 데이터를 처리하는 로직을 추가
-    // 예를 들어, 데이터베이스에 저장하거나 다른 클라이언트들에게 브로드캐스팅할 수 있습니다.
-
-    // 클라이언트에게 응답을 보내고 처리 완료를 알림
+  socket.on('message', (charId, value, callback) => {
+    console.log('message :', charId, value);
     callback('Data received and processed successfully');
-    io.emit("receive message", data);
+    io.emit("receive message", charId, value);
   });
 });
 
 httpServer.listen(8282);
-//io.listen(8282);
